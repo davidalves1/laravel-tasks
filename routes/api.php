d692646633eba30313f57ade5e46e2e38549fd62
David Alves de Souza <alves.david@outlook.com>
2018-10-01 15:47:52 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/', function () {
     error_log("INFO: get /");
-    return response()->json(['msg' => 'Success');
+    return response()->json(['msg' => 'Success']);
 });
